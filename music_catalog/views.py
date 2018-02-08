929fc7da6243e61aaa5fb0f640a3aabb85d453f2
Alexey Kurylev <chepe4pi@gmail.com>
2018-02-08 18:31:50 +0300

    add models and app

--- music_catalog/views.py
+++ music_catalog/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render_to_response
 
-# Create your views here.
+from music_catalog.models import Song
+
+
+def main_view(request):
+    songs = Song.objects.all()
+    return render_to_response('main_template.html', context={'songs': songs})
--- wrorkshop/urls.py
+++ wrorkshop/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from music_catalog.views import main_view
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', main_view),
 ]
